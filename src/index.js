71a5c8d431633efd8578e2f1bab62e20039548c2
정용인 <lordofbabo@colligence.io>
2021-02-08 10:34:33 +0900

    npm mysql uninstall, mariadb connection pool setting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+const mdbConn = require('./mariaDBConn');
 const Koa = require('koa');
 const Router = require('koa-router');
 
 const api = require('./api');
+mdbConn.getUserList()
+.then((rows) => {
+    console.log(rows);
+})
+.catch((errMsg) => {
+    console.log(errMsg);
+});
 
 const app = new Koa();
 const router = new Router();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 //router 설정
 router.use('/api', api.routes());
