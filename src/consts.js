5ed47c1658d0fbf749cb6ade4fd1279889328451
정용인 <lordofbabo@colligence.io>
2021-02-19 13:51:04 +0900

    banana setting

--- src/consts.js
+++
@@ -1,9 +0,0 @@
-module.exports = 
-(function() { 
-      return { 
-               DBHost: "localhost",
-               DBPort : 80,  
-               DBUser: "root", 
-               DBPass: "1234", 
-              } 
-           })();
