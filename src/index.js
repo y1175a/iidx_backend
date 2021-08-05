0c5db5fc9d8483d84de53b5fa1fefb653a416cf9
Sangsu Baek <skygana@gmail.com>
2021-08-06 00:19:54 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,27 +3,23 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const session = require('koa-session');
+const cors = require('@koa/cors');
 const passport = require('koa-passport');
 const passportStrategy = require('./passport/strategy');
 const passportSerialize = require('./passport/serialize');
-const cors = require('@koa/cors');
-
 const api = require('./api');
-
 const app = new Koa();
 const router = new Router();
-
 const port = process.env.PORT;
 
 //router 설정
 router.use('/api', api.routes());
 
+//passport 전략 사용
 passportStrategy();
 
-//app 인스턴스에 라우터 적용
 app.use(bodyParser({
-  formLimit: "10mb",
-  jsonLimit: "10mb"
+  formLimit: "2mb",
 }));
 app.keys = [process.env.CLIENT_SECRET];
 app.use(session({
@@ -34,14 +30,11 @@ app.use(session({
 }, app));
 app.use(passport.initialize()).use(passport.session());
 app.use(router.routes()).use(router.allowedMethods());
-app.use(
-    cors({
-      origin: "http://localhost:3000",
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      credentials: true
-    })
-  );
+app.use(cors());
 passportSerialize();
+
 app.listen(port, () => {
     console.log('::listening to port ' + port + '::');
 })
+
+module.exports = app;
