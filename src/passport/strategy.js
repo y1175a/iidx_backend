c95f0696fab44f6a0b80727ee93181ea16555852
Sangsu Baek <skygana@gmail.com>
2021-08-07 20:40:39 +0900

    Update strategy.js
    existUser, newUser 수정

--- src/passport/strategy.js
+++ src/passport/strategy.js
@@ -1,6 +1,6 @@
 const passport = require('koa-passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const { Users } = require('../database/models');
+const { User } = require('../database/models');
 
 module.exports = () => {
     passport.use(new GoogleStrategy({
@@ -10,17 +10,16 @@ module.exports = () => {
         proxy: true
     }, async (accessToken, refreshToken, profile, done) => {
             try {
-                const existUser = await Users.findOne({
-                    where: { id: profile.id }
+                const existUser = await User.findOne({
+                    where: { u_uid: profile.id }
                 });
                 if (existUser) {
                     return done(null, existUser);
                 } else {
-                    const newUser = await Users.create({
-                        id: profile.id,
-                        email: profile._json && profile._json.email,
-                        roletype: 'user',
-                        nickname: '',
+                    const newUser = await User.create({
+                        u_uid: profile.id,
+                        u_email: profile._json && profile._json.email,
+                        u_nick: '',
                     });
                     return done(null, newUser);
                 }
